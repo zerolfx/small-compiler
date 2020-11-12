ece596e35b1cc270f3aeb42e8c99a10c5e856c5d
Fangxin Lin <fangxin.lin@tusimple.ai>
2020-11-12 18:44:21 +0800

    add: can work as a compiler

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include "compiler.hpp"
+#include <fstream>
 
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cout << "Usage: " << argv[0] << " input-file output-file" << std::endl;
+    return 1;
+  }
+  std::ifstream ifs{argv[1]};
+  std::string in{std::istreambuf_iterator<char>{ifs}, {}};
+  ifs.close();
 
-int main() {
-  auto in = R"|(
-    i := 1;
-    match i of
-      case 1 => write 1
-      case 2 => write 2
-    end
-)|";
-  compile(in);
+  std::ofstream ofs{argv[2]};
+  ofs << compile(in);
+  ofs.close();
 }
